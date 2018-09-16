c2b0a4efa9009a3d2147509a62003b2c76e538a4
陈国强 <407339708@qq.com>
2018-09-16 12:23:07 +0800

    leet

---
+++ src/LeetCode/primary_level/isPalindrome.java
@@ -0,0 +1,8 @@
+package LeetCode.primary_level;
+
+public class isPalindrome {
+    public static void main(String args[]){
+        System.out.println(' '== 32);
+    }
+}
+
---
+++ src/LeetCode/primary_level/theSameWord.java
@@ -0,0 +1,21 @@
+package LeetCode.primary_level;
+
+import java.util.*;
+
+public class theSameWord {
+    public boolean isAnagram(String s, String t) {
+        if (s.length()!=t.length()) return false;
+        HashMap<Character,Integer> map1 = new HashMap<Character, Integer>();
+        HashMap<Character,Integer> map2 = new HashMap<Character, Integer>();
+        char[] ch1 = s.toCharArray();
+        char[] ch2 = t.toCharArray();
+        for (int i=0;i<s.length();i++){
+            if (!map1.containsKey(ch1[i])){map1.put(ch1[i],1);}
+            else map1.replace(ch1[i],map1.get(ch1[i])+1);
+            if (!map2.containsKey(ch2[i])){map2.put(ch2[i],1);}
+            else map2.replace(ch2[i],map2.get(ch2[i])+1);
+        }
+        if (map1.equals(map2))return true;
+        else return false;
+    }
+}
