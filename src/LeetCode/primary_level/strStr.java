ff5860c25bc48cdbbe83ff87bdbf2a2756ccd75f
陈国强 <407339708@qq.com>
2018-09-19 10:35:13 +0800

    leetPrac

--- src/LeetCode/primary_level/strStr.java
+++ src/LeetCode/primary_level/strStr.java
@@ -2,18 +2,25 @@ package LeetCode.primary_level;
 
 public class strStr {
     public static void main(String args[]){
-
+        String first = "a";
+        String second = "";
+        strStr ss = new strStr();
+        System.out.println(ss.strStr(first,second));
     }
 
     public int strStr(String haystack, String needle) {
         if (needle=="")return 0;
         if (haystack.length()==0&(needle.length()!=0))return -1;
-        int start=0;int end=0;
+        int count;int start = 0;
         for (int i=0;i<haystack.length();i++){
+            count=0;
             if (haystack.charAt(i)==needle.charAt(0))
             for (int j=0;j<needle.length();j++){
-                
+                if (haystack.charAt(i+j)==needle.charAt(j))
+                    count++;
+                if (count==needle.length()) start=i;
             }
         }
+        return start;
     }
 }
