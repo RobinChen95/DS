a33eff8beecbabfe55220c1e6edb75f10eb7c27b
陈国强 <407339708@qq.com>
2018-09-21 11:32:14 +0800

    leetprac

--- src/LeetCode/primary_level/ListAndTree/deleteNode.java
+++ src/LeetCode/primary_level/ListAndTree/deleteNode.java
@@ -8,6 +8,13 @@ public class deleteNode {
     }
 
     public void deleteNode(ListNode node) {
-        
+
+    }
+
+    public static void main(String args[]){
+        int[] head = {4,5,1,9};
+        for (int i=0;i<head.length;i++){
+            
+        }
     }
 }
