8223fafed9c055bbaf58429a0f73fae8fefc4ba8
陈国强 <407339708@qq.com>
2018-09-25 22:52:58 +0800

    leetPrac

--- src/LeetCode/primary_level/ListAndTree/reverseList.java
+++ src/LeetCode/primary_level/ListAndTree/reverseList.java
@@ -1,4 +1,26 @@
 package LeetCode.primary_level.ListAndTree;
 
 public class reverseList {
+
+    private int size=0;
+
+    public class Node{
+        int val;
+        Node next;
+        Node(int x){val=x;}
+    }
+
+    public Node reverseList(Node head) {
+        if (head == null) return null;
+        Node p1 = head;
+        Node p2 = head.next;
+        p1.next = null;
+        while(p2!=null){
+            Node temp = p2.next;
+            p2.next=p1;
+            p1=p2;
+            p2=temp;
+        }
+        return p1;
+    }
 }
