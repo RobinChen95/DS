68ebe423755b94ae6aeb0e3292bb2b441823f41b
陈国强 <407339708@qq.com>
2018-10-15 17:26:48 +0800

    Nothing

--- src/Chapter1_Basic/Test.java
+++ src/Chapter1_Basic/Test.java
@@ -1,27 +1,9 @@
 package Chapter1_Basic;
 
-import edu.princeton.cs.algs4.*;
 
 public class Test {
-    public static void main(String[] args){
-        int[] a = new int[3];
-        a[0]=1;
-        a[1]=2;
-        a[2]=3;
-        for(int num : a){
-            System.out.print(num);
-        }
-    }
-    public int maxProfit(int[] prices) {
-        if (prices.length == 0) return 0;
-        int min = prices[0];
-        int sum = 0;
-        for (int i = 1; i < prices.length; i++) {
-            if (min < prices[i]) {
-                sum += prices[i] - min;
-                min = prices[i];
-            } else min = prices[i];
-        }
-        return sum;
+    public static void main(String[] args) {
+        System.out.println(Float.MAX_VALUE);
+
     }
 }
