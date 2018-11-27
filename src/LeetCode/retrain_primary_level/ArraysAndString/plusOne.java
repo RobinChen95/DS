64a1dfaa2f2af54789cf66803ff2dd9bcbf9201c
陈国强 <407339708@qq.com>
2018-11-27 21:43:21 +0800

    leetprac

--- src/LeetCode/retrain_primary_level/ArraysAndString/plusOne.java
+++ src/LeetCode/retrain_primary_level/ArraysAndString/plusOne.java
@@ -29,28 +29,51 @@ public class plusOne {
         ArrayList<Integer> al = new ArrayList<>();
         //检查进位
         boolean[] check = new boolean[digits.length];
-        for (int i = 0; i < check.length; i++) {
-            check[i]=false;
-        }
+        //尾号加一
         digits[digits.length-1]++;
-        //这一段需要重写
-        for (int i = 0; i < digits.length-1; i++) {
+        if (digits[digits.length-1]==10){
+            check[digits.length-1]=true;
+            digits[digits.length-1]=0;
+        }
+        al.add(digits[digits.length-1]);
+        for (int i = digits.length-1; i > 0; i--) {
             if (check[i]){
                 //如果i进位
-                digits[i]++;
+                digits[i-1]++;
             }
-            if (digits[i]==10){
-                digits[i]=0;
-                //如果i位进位，将check[i+1]置为true
-                check[i+1]=true;
+            if (digits[i-1]==10){
+                digits[i-1]=0;
+                //如果i位进位，将check[i-1]置为true
+                check[i-1]=true;
+            }
+            al.add(digits[i-1]);
+        }
+        if (check[0])al.add(1);
+        int[] result = new int[al.size()];
+        for (int i = 0; i < al.size(); i++) {
+            result[i] = al.get(al.size()-i-1);
+        }
+        return result;
+    }
+
+    //github上的最佳解法
+    public int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0 ; i--) {
+            if (digits[i] != 9) {
+                digits[i]++;
+                return digits;
+            } else {
+                digits[i] = 0;
             }
         }
-        return digits;
+        int[] result = new int[digits.length + 1];
+        result[0] = 1;
+        return result;
     }
 
     public static void main(String[] args) {
         int[] a1 = {6,1,4,5,3,9,0,1,9,5,1,8,6,7,0,5,5,4,3};
-        int[] a2 = {6,1,4,5,3,9,0,1,9,5,1,8,6,7,0,5,5,4,3};
+        int[] a2 = {9};
         plusOne po = new plusOne();
         System.out.println(Arrays.toString(po.plusOne1(a1)));
         System.out.println(Arrays.toString(po.plusOne2(a2)));
