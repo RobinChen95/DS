22ed18fe52a9aad9f76a55f25e95d34102a3d140
陈国强 <407339708@qq.com>
2018-12-09 17:27:59 +0800

    修改笔记

--- src/ChenGQ/test.java
+++ src/ChenGQ/test.java
@@ -1,29 +1,19 @@
 package ChenGQ;
 
 import java.io.*;
+import java.util.HashMap;
 
 public class test {
     // 测试分支
-    public static void main(String[] args) throws Exception {
-        File file = new File("/Users/chenguoqiang/IdeaProjects/JavaMQ/src/main/java/ChenGQ/data/test.txt");
-        System.out.println(readFile(file));
-    }
+    public static void main(String[] args) {
+        final HashMap<Integer,String> hm = new HashMap<>();
+        //这句话会报错 hm = new HashMap<>();
+        hm.put(1,"第1次");
+        hm.put(2,"第2次");
+        hm.put(3,"第3次");
 
-    public static String readFile(File file) throws Exception {
-        StringBuilder result =new StringBuilder();
-        try {
-            BufferedReader br =new BufferedReader(new FileReader(file));
-            String s =null;
-            while((s =br.readLine()) != null) { //一次读一行内容
-                result.append(System.lineSeparator() +s);
-            }
-            br.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return result.toString();
+        hm.remove(1);
+        System.out.println(hm.toString());
     }
 
 
