799ee47b2abeac9b73a855d5f044221df3163245
陈国强 <407339708@qq.com>
2018-12-24 17:44:56 +0800

    Java复习

--- src/Chapter1_Basic/Test.java
+++ src/Chapter1_Basic/Test.java
@@ -1,56 +1,9 @@
 package Chapter1_Basic;
 
-
-import java.util.ArrayList;
-import java.util.Stack;
-
 public class Test {
-    public static boolean isValid(String s) {
-        if (s == null || "".equals(s)) {
-            return true;
+    public static void main(String[] args) {
+        int monthDays[];
+        monthDays =new int[]{ 31,28,31,30,31,30,31,31,30,31,30,31};
         }
-        if (s.length() % 2 != 0) {
-            return false;
-        }
-        Stack stack = new Stack();
-        for (int i = 0; i < s.length(); i++) {
-            String s1 = String.valueOf(s.charAt(i));
-            String pop;
-            if ("(".equals(s1) || "{".equals(s1) || "[".equals(s1)) {
-                stack.push(s1);
-            } else if (")".equals(s1)) {
-                if (stack.size() < 1) {
-                    return false;
-                } else {
-                    pop = (String) stack.pop();
-                }
-                if (!"(".equals(pop)) {
-                    return false;
-                }
-            } else if ("}".equals(s1)) {
-                if (stack.size() < 1) {
-                    return false;
-                } else {
-                    pop = (String) stack.pop();
-                }
-                if (!"{".equals(pop)) {
-                    return false;
-                }
-            } else if ("]".equals(s1)) {
-                if (stack.size() < 1) {
-                    return false;
-                } else {
-                    pop = (String) stack.pop();
-                }
-                if (!"[".equals(pop)) {
-                    return false;
-                }
-            }
-        }
-        if (!stack.empty()) {
-            return false;
-        }
-        return true;
-    }
 
-}
+    }
