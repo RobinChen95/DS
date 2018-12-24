c077b666f87b4901a411dd74bd7a88775067ec9b
chenguoqiang <407339708@qq.com>
2018-12-24 21:41:34 +0800

    java笔记

--- src/Chapter1_Basic/Test.java
+++ src/Chapter1_Basic/Test.java
@@ -1,9 +1,9 @@
 package Chapter1_Basic;
 
-public class Test {
+public class Test extends Thread {
+    volatile boolean finished;
     public static void main(String[] args) {
-        int monthDays[];
-        monthDays =new int[]{ 31,28,31,30,31,30,31,31,30,31,30,31};
+
         }
 
     }
