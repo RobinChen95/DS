83866e936ea9b06f343b80296b7a601be907faf4
chenguoqiang <407339708@qq.com>
2018-12-25 15:26:04 +0800

    java复习笔记

--- src/Chapter1_Basic/Test.java
+++ src/Chapter1_Basic/Test.java
@@ -1,9 +1,13 @@
 package Chapter1_Basic;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+
 public class Test extends Thread {
     volatile boolean finished;
     public static void main(String[] args) {
-
+        Collection<Integer> c = new HashSet<>();
         }
 
     }
