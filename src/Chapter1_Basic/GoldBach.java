ad74c5dd4b04ac6e5e73137d6c62c63e2f8af3f8
chenguoqiang <407339708@qq.com>
2019-01-11 16:53:15 +0800

    添加备忘录

--- src/Chapter1_Basic/GoldBach.java
+++ src/Chapter1_Basic/GoldBach.java
@@ -1,13 +1,26 @@
 package Chapter1_Basic;
 
+/*
+* 关于哥德巴赫猜想的穷举法证明
+* 做了断点续训
+* 性能需要
+*   1、多线程优化
+* */
+
 import java.io.*;
 import java.math.BigInteger;
 
 public class GoldBach {
+    //建立备忘录队列
+    static BigInteger[] latest_used = new BigInteger[100];
+    static int pointer = -1;
 
     public static void main(String[] args) {
+        for (int i = 0; i < latest_used.length; i++) {
+            latest_used[i]=BigInteger.valueOf(0);
+        }
         try {
-            BigInteger bigInteger = new BigInteger("113731378");
+            BigInteger bigInteger = new BigInteger("123232318");
             FileReader fr = new FileReader("./GoldBach.txt");
             BufferedReader br = new BufferedReader(fr);
             String savedNum = br.readLine();
@@ -49,10 +62,23 @@ public class GoldBach {
     }
 
     public static boolean isPrime(BigInteger bi){
+        //大于10000时查询备忘录
+        if (bi.compareTo(BigInteger.valueOf(10000))>0){
+            for (int i = 0; i < latest_used.length; i++) {
+                if (bi.compareTo(latest_used[i])==0){return true;}
+            }
+        }
         for (BigInteger bigInteger=BigInteger.valueOf(2);bigInteger.compareTo(bi.sqrt())<=0;
              bigInteger=bigInteger.add(BigInteger.valueOf(1))) {
             if (bi.mod(bigInteger)==BigInteger.valueOf(0)){return false;}
         }
+        //大于10000时存入备忘录队列
+        if (bi.compareTo(BigInteger.valueOf(10000))>0){
+            pointer+=1;
+            pointer = pointer%100;
+            latest_used[pointer] = bi;
+        }
         return true;
     }
+
 }
