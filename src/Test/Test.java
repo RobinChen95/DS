acfefd3983f2a2dad76da492ebb59e74db19b5a9
陈国强 <407339708@qq.com>
2019-01-24 12:11:57 +0800

    部分修正

--- src/Test/Test.java
+++ src/Test/Test.java
@@ -4,10 +4,23 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
 
-public class Test extends Thread {
-    volatile boolean finished;
-    public static void main(String[] args) {
-        Collection<Integer> c = new HashSet<>();
-        }
+public class Test implements Runnable {
 
+    int n = 5;
+
+    @Override
+    public void run() {
+        while(n!=0){
+            System.out.println("the Thread never stops!");
+            System.out.println("n:"+n);
+            n--;
+        }
     }
+
+    public static void main(String[] args) {
+        Test t1 = new Test();
+        t1.run();
+        Test t2 = t1;
+        t2.run();
+    }
+}
